741d0a0ce64f43048b3ead6c999433d86497e91c
Abdelaali Es salmi <aes-salm@e1r10p6.1337.ma>
2021-04-27 17:26:10 +0000

    de

--- functions/ps_functions/find_median.c
+++ functions/ps_functions/find_median.c
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 12:27:36 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/04/26 13:56:50 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/04/27 15:55:44 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,12 @@
 int find_median(int *array, int len)
 {
     int median;
-    int i;
-    int array_copy[len];
-
-    i = -1;
-    while (++i < len)
-        array_copy[i] = array[i];
-
-    sort_array(array_copy, len);
-
+    int *sorted;
+    sorted = sort_array(array, len);
     if(len % 2 == 0)
-        median = (array_copy[(len-1)/2] + array_copy[len/2])/2;
+        median = (sorted[(len-1)/2] + sorted[len/2])/2;
     else
-        median = array_copy[len/2];
-
-    return median;
+        median = sorted[len/2];
+    free(sorted);
+    return (median);
 }
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 16:33:44 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/04/26 16:21:48 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/04/27 16:46:00 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,10 @@ void my_quicksort(t_stack *a, t_stack *b, int len)
     int i;
     int median;
     
-    // if (a->top == -1)
-    //     return
-
-    median = find_median(a->items, a->top);
-    printf("%d\n", median);
+    if (len == 1)
+        return;
+    median = find_median(a->items, len + 1);
+    // printf("%d\n", median);
     
     i = -1;
     while(++i <= len)
@@ -31,12 +30,30 @@ void my_quicksort(t_stack *a, t_stack *b, int len)
         if (a->items[a->top] > median)
         {
             push_stack(b, a);
-            write(1, "pb\n", 3);
+            // write(1, "pb\n", 3);
             top_half_len++;
         }
-        rotate_stack(a);
-        write(1, "ra\n", 3);
+        reverse_rotate_stack(a);
+        // write(1, "rra\n", 3);
     }
+    
+    //reverse the list back to original position
+    i = -1;
+    while(++i <= (len - top_half_len))
+    {
+        reverse_rotate_stack(a);
+        // write(1, "rra\n", 3);
+    }
+    //push larger half onto smaller half
+    i = -1;
+    while(++i <= top_half_len)
+    {
+        push_stack(a, b);
+        // write(1, "pa\n", 3);
+    }
+    
+    my_quicksort(a, b, top_half_len);
+    
 }
 
 int get_index(int *array, int el, int len)
@@ -61,6 +78,8 @@ int main(int len, char **args)
     t_stack    b;
     int i;
     int *sorted;
+    int median;
+    int top_half_len;
     
     if (len == 1)
         return (1);
@@ -74,31 +93,62 @@ int main(int len, char **args)
         print_error("There Are Duplicates\n");
 
     // my_quicksort(&a, &b, a.top);
-    sorted = sort_array(a.items, a.top);
-    // i = - 1;
-    // while (++i <= a.top)
-    //     printf("%d ", sorted[i]);
-    // printf("\n");
 
+    median = find_median(a.items, a.top + 1);
+    // printf("%d\n", median);
+    
+    top_half_len = 0;
     i = -1;
-    while (++i < len)
+    while(++i <= len)
     {
-        while (get_index(a.items, sorted[i], len) != a.top)
+        if (a.items[a.top] > median)
         {
-            rotate_stack(&a);
-            write(1, "ra\n", 3);
+            push_stack(&b, &a);
+            write(1, "pb\n", 3);
+            top_half_len++;
         }
+        reverse_rotate_stack(&a);
+        write(1, "rra\n", 4);
+    }
+
+    sorted = sort_array(b.items, b.top);
+
+    i = top_half_len;
+    while (--i > -1 )
+    {
+        while (get_index(b.items, sorted[i], b.top) != b.top)
+        {
+            rotate_stack(&b);
+            write(1, "rb\n", 3);
+        }
+        push_stack(&a, &b);
+        write(1, "pa\n", 3);
+    }
+    i = -1;
+    while (++i < top_half_len)
+    {
+        rotate_stack(&a);
+        write(1, "ra\n", 3);
+    }
+    i = -1;
+    while (++i < (len - top_half_len))
+    {
         push_stack(&b, &a);
         write(1, "pb\n", 3);
     }
-    i = -1;
-    while (++i < len)
+
+    sorted = sort_array(b.items, b.top);
+    while (--i > -1)
     {
+        while (get_index(b.items, sorted[i], b.top) != b.top)
+        {
+            rotate_stack(&b);
+            write(1, "rb\n", 3);
+        }
         push_stack(&a, &b);
+        write(1, "pa\n", 3);
     }
     
-
-
 
 
     //// print stacks ///////
