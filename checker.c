e0a4f9d51eddbb519ba6b80b4b9c147b049b45ec
Abdelaali Es salmi <aes-salm@e1r10p6.1337.ma>
2021-05-06 14:30:21 +0000

    finish the norm-v3

--- checker.c
+++ checker.c
@@ -6,58 +6,63 @@
 /*   By: aes-salm <aes-salm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/07 18:46:26 by aes-salm          #+#    #+#             */
-/*   Updated: 2021/05/05 14:47:23 by aes-salm         ###   ########.fr       */
+/*   Updated: 2021/05/06 14:23:45 by aes-salm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/push_swap.h"
 
-int main(int len, char **args)
+/*
+** print stucks values
+*/
+void	print_stucks(t_stack *a, t_stack *b)
 {
-    t_stack a;
-    t_stack b;
+	int	i;
 
-    char    *operation;
-    int     i;
-    
-    if (len < 2)
-        return (1);
-    len = len - 1;
-    newStack(&a, len);
-    newStack(&b, len);
-    i = len;
-    while (--i >= 0)
-        push(&a, ft_atoi(args[i+1]));
-        
-    if (duplicates_check(a.items, len))
-        print_error("There Are Duplicates\n");
-    
-    if (a.size == 1)
-        return (0);
+	i = -1;
+	printf("\na | ");
+	while (++i <= a->top)
+		printf("%d ", a->items[i]);
+	printf("\n");
+	printf("b | ");
+	i = -1;
+	while (++i <= b->top)
+		printf("%d ", b->items[i]);
+	printf("\n\n");
+}
 
-    operation = NULL;
-    while (get_operation(&operation) > 0)
+void	check_stucks(t_stack *a, t_stack *b)
+{
+	if (is_sorted(a, b))
+		write(1, "OK\n", 3);
+	else
+		write(1, "KO\n", 3);
+}
+
+int	main(int len, char **args)
+{
+	t_stack	a;
+	t_stack	b;
+	char	*operation;
+	int		i;
+
+	if (len < 2)
+		return (1);
+	len = len - 1;
+	newStack(&a, len);
+	newStack(&b, len);
+	i = len;
+	while (--i >= 0)
+		push(&a, ft_atoi(args[i + 1]));
+	if (a.size == 1)
+		return (0);
+	if (duplicates_check(a.items, len))
+		print_error("There Are Duplicates\n");
+	while (get_operation(&operation) > 0)
 		applicate_the_operation(operation, &a, &b);
-    if (operation[0] != '\0')
-        applicate_the_operation(operation, &a, &b);
-
-
-    /// print stacks ///////
-    i = - 1;
-    printf("\na | ");
-    while (++i <= a.top)
-        printf("%d ", a.items[i]);
-    printf("\n");
-    printf("b | ");
-    i = - 1;
-    while (++i <= b.top)
-        printf("%d ", b.items[i]);
-    printf("\n\n");
-    // /// end print stacks ///////
-
-    if (is_sorted(&a, &b))
-        write(1, "OK\n", 3);
-    else
-        write(1, "KO\n", 3);
-    return (0);
+	if (operation[0] != '\0')
+		applicate_the_operation(operation, &a, &b);
+	print_stucks(&a, &b);
+	check_stucks(&a, &b);
+	return (0);
 }
